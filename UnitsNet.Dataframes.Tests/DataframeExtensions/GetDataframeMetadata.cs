87964ef2c933b04eeb674800f17f945421973bfb
Lenny Kean <lennykean@gmail.com>
2023-06-06 21:38:49 -0700

    test GetDataframeMetadata

--- UnitsNet.Dataframes.Tests/DataframeExtensions/GetDataframeMetadata.cs
+++ UnitsNet.Dataframes.Tests/DataframeExtensions/GetDataframeMetadata.cs
@@ -1,10 +1,9 @@
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
-using Newtonsoft.Json;
-
 using NUnit.Framework;
 
 using UnitsNet.Dataframes.Tests.TestData;
+using UnitsNet.Units;
 
 namespace UnitsNet.Dataframes.Tests.DataframeExtensions;
 
@@ -18,7 +17,23 @@ public class GetDataframeMetadata
 
         var metadata = box.GetDataframeMetadata();
 
-        var json = JsonConvert.SerializeObject(metadata);
-
+        Assert.Multiple(() =>
+        {
+            Assert.That(metadata, Has.Count.EqualTo(7));
+            Assert.That(metadata, Has.ItemAt(nameof(Box.Width))
+                .Property(nameof(QuantityMetadata.Unit)).Property(nameof(UnitMetadata.UnitInfo)).Property(nameof(UnitInfo.Value)).EqualTo(LengthUnit.Meter));
+            Assert.That(metadata, Has.ItemAt(nameof(Box.Height))
+                .Property(nameof(QuantityMetadata.Unit)).Property(nameof(UnitMetadata.UnitInfo)).Property(nameof(UnitInfo.Value)).EqualTo(LengthUnit.Meter));
+            Assert.That(metadata, Has.ItemAt(nameof(Box.Depth))
+                .Property(nameof(QuantityMetadata.Unit)).Property(nameof(UnitMetadata.UnitInfo)).Property(nameof(UnitInfo.Value)).EqualTo(LengthUnit.Meter));
+            Assert.That(metadata, Has.ItemAt(nameof(Box.Weight))
+                .Property(nameof(QuantityMetadata.Unit)).Property(nameof(UnitMetadata.UnitInfo)).Property(nameof(UnitInfo.Value)).EqualTo(MassUnit.Kilogram));
+            Assert.That(metadata, Has.ItemAt(nameof(Box.Items))
+                .Property(nameof(QuantityMetadata.Unit)).Property(nameof(UnitMetadata.UnitInfo)).Property(nameof(UnitInfo.Value)).EqualTo(ScalarUnit.Amount));
+            Assert.That(metadata, Has.ItemAt(nameof(Box.Data))
+                .Property(nameof(QuantityMetadata.Unit)).Property(nameof(UnitMetadata.UnitInfo)).Property(nameof(UnitInfo.Value)).EqualTo(InformationUnit.Bit));
+            Assert.That(metadata, Has.ItemAt(nameof(Box.Volume))
+                .Property(nameof(QuantityMetadata.Unit)).Property(nameof(UnitMetadata.UnitInfo)).Property(nameof(UnitInfo.Value)).EqualTo(VolumeUnit.CubicMeter));
+        });
     }
 }
--- UnitsNet.Dataframes.Tests/TestData/IBox.cs
+++ UnitsNet.Dataframes.Tests/TestData/IBox.cs
@@ -1,14 +1,17 @@
-﻿namespace UnitsNet.Dataframes.Tests.TestData;
+﻿using UnitsNet.Dataframes.Attributes;
+using UnitsNet.Units;
+
+namespace UnitsNet.Dataframes.Tests.TestData;
 
 public interface IBox
 {
-    string? Data { get; set; }
-    double Depth { get; set; }
-    double Height { get; set; }
-    int Items { get; set; }
-    int Priority { get; set; }
     int SerialNumber { get; set; }
-    double Volume { get; }
-    double Weight { get; set; }
+    int Priority { get; set; }
     double Width { get; set; }
+    double Height { get; set; }
+    double Depth { get; set; }
+    double Weight { get; set; }
+    int Items { get; set; }
+    string? Data { get; set; }
+    double Volume { get; }
 }
